eb508854ba31695c9d67794a7ec8709ae13fed4f
Iris Chen <jaminitaa0322@gmail.com>
2025-02-25 14:19:03 -0800

    update the images to cloudinary

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,29 +1,47 @@
 import { useRef, useState, useEffect }  from 'react'
 import ProjectsDetail from './ProjectsDetail';
 import { motion } from "framer-motion";
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
+import { AdvancedImage } from '@cloudinary/react';
 
 const ProjectCards = () => {
   // Modal states
   const [modalViewOpen, setModalViewOpen] = useState(false)
   const [selectedProject, setSelectedProject] = useState(null)
 
+  const cld = new Cloudinary({ cloud: { cloudName: 'dwqse2aia' } });
+
   const projects = [
     {
       title: "Coala",
       description: "B2B comprehensive mobile app that helps coaches to manage their progress through automated curriculum, attendance, evaluation, and reporting all within one app",
-      image: "../../public/projects/Coala.png",
+      image: cld
+        .image('Coala_puubtc')
+        .format('auto')
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(500)),
       link: "https://main.d1tudobvnhl6lq.amplifyapp.com/"
     },
     {
       title: "Credence",
       description: "B2B single-page application of an all-in-one CRM platform that helps small businesses and freelancers in business manage their business effortlessly, with functions of email scheduling, client and note creation, email retrieval, report graphs and search functionality.",
-      image: "../../public/projects/project-credence-calendar.jpg",
+      image: cld
+        .image('project-credence-calendar_eswwb6')
+        .format('auto')
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(300)),
       link: "https://credence.onrender.com/"
     },
     {
       title: "Pin.Co",
       description: "A system that helps people to do safety check-ins, share real-time location with friends, send sos messages to contacts by one-click button and ask call simulation to keep safe.",
-      image: "../../public/projects/Pinco.png",
+      image: cld
+        .image('Pinco_pheo8e')
+        .format('auto')
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(300)),
       link: "#"
     }
   ];
@@ -42,7 +60,10 @@ const ProjectCards = () => {
           onClick={() => toggleViewModal(project)} 
           className="w-full md:w-1/3 bg-white/20 backdrop-blur-lg rounded-lg p-2 md:p-6 flex flex-col gap-4 transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl font-mono"
         >
-          <img className="h-50 md:h-50 mt-3 object-cover rounded-xl" src={project.image} alt={project.title}/>
+          <AdvancedImage 
+            cldImg={project.image} 
+            className="h-50 md:h-50 mt-3 object-cover rounded-xl"
+          />
           <div className="p-2 flex flex-col flex-grow">
             <h2 className="font-bold text-xl mb-3 text-yellow-500">{project.title}</h2>
             <p className="text-base leading-relaxed text-gray-800 flex-grow">
@@ -78,9 +99,8 @@ const ProjectCards = () => {
           />
           <div className="bg-white p-6 rounded-lg shadow-xl relative max-w-md w-full m-4 font-mono">
             <h2 className="text-2xl font-bold mb-4 text-yellow-500">{selectedProject.title}</h2>
-            <img 
-              src={selectedProject.image} 
-              alt={selectedProject.title} 
+            <AdvancedImage 
+              cldImg={selectedProject.image}
               className="w-full h-48 object-cover rounded-lg mb-4"
             />
             <p className="text-gray-700 mb-4">{selectedProject.description}</p>
